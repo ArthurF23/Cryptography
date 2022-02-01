e6df9b00004c186f00748876afc71f22ddbe3be9
Arthur <71910656+ArthurF23@users.noreply.github.com>
2022-02-01 19:33:01 +0000

    Started implementation of vigenere cypher

--- Headers/Encryption/encryption.cpp
+++ Headers/Encryption/encryption.cpp
@@ -669,4 +669,199 @@ namespace encryption {
     //AES init
     encryption::AES::aes_init(genKey, aesKey);
   };
+
+  /////////////////////
+  ///Vigenere Cypher///
+  /////////////////////
+
+  char translate(char message) {
+    switch (message) {
+    case 'A':
+      return 0;
+    case 'B':
+      return 1;
+    case 'C':
+      return 2;
+    case 'D':
+      return 3;
+    case 'E':
+      return 4;
+    case 'F':
+      return 5;
+    case 'G':
+      return 6;
+    case 'H':
+      return 7;
+    case 'I':
+      return 8;
+    case 'J':
+      return 9;
+    case 'K':
+      return 10;
+    case 'L':
+      return 11;
+    case 'M':
+      return 12;
+    case 'N':
+      return 13;
+    case 'O':
+      return 14;
+    case 'P':
+      return 15;
+    case 'Q':
+      return 16;
+    case 'R':
+      return 17;
+    case 'S':
+      return 18;
+    case 'T':
+      return 19;
+    case 'U':
+      return 20;
+    case 'V':
+      return 21;
+    case 'W':
+      return 22;
+    case 'X':
+      return 23;
+    case 'Y':
+      return 24;
+    case 'Z':
+      return 25;
+
+
+    case 'a':
+      return 0;
+    case 'b':
+      return 1;
+    case 'c':
+      return 2;
+    case 'd':
+      return 3;
+    case 'e':
+      return 4;
+    case 'f':
+      return 5;
+    case 'g':
+      return 6;
+    case 'h':
+      return 7;
+    case 'i':
+      return 8;
+    case 'j':
+      return 9;
+    case 'k':
+      return 10;
+    case 'l':
+      return 11;
+    case 'm':
+      return 12;
+    case 'n':
+      return 13;
+    case 'o':
+      return 14;
+    case 'p':
+      return 15;
+    case 'q':
+      return 16;
+    case 'r':
+      return 17;
+    case 's':
+      return 18;
+    case 't':
+      return 19;
+    case 'u':
+      return 20;
+    case 'v':
+      return 21;
+    case 'w':
+      return 22;
+    case 'x':
+      return 23;
+    case 'y':
+      return 24;
+    case 'z':
+      return 25;
+    default:
+      return -1; //exclude char
+    }
+  }
+
+  char vigenere::translate(char message, char key) {
+    int key_row;
+    int letter_num;
+    //Find Key Row
+    for (int i = 0; i < 26; i++) {
+      if (key == vigenere::table[i][0]) {
+        key_row = i;
+        break;
+      }
+    }
+    //Find Letter In Key Row
+    for (int i = 0; i < 26; i++) {
+      if (vigenere::table[key_row][i] == message) {
+        letter_num = i;
+      }
+    }
+
+    switch (letter_num) {
+    case 0:
+      return 'A';
+    case 1:
+      return 'B';
+    case 2:
+      return 'C';
+    case 3:
+      return 'D';
+    case 4:
+      return 'E';
+    case 5:
+      return 'F';
+    case 6:
+      return 'G';
+    case 7:
+      return 'H';
+    case 8:
+      return 'I';
+    case 9:
+      return 'J';
+    case 10:
+      return 'K';
+    case 11:
+      return 'L';
+    case 12:
+      return 'M';
+    case 13:
+      return 'N';
+    case 14:
+      return 'O';
+    case 15:
+      return 'P';
+    case 16:
+      return 'Q';
+    case 17:
+      return 'R';
+    case 18:
+      return 'S';
+    case 19:
+      return 'T';
+    case 20:
+      return 'U';
+    case 21:
+      return 'V';
+    case 22:
+      return 'W';
+    case 23:
+      return 'X';
+    case 24:
+      return'Y';
+    case 25:
+      return 'Z';
+    }
+  }
+
+  string vigenere::remove_spaces(string inp) {
+    inp.erase(remove(inp.begin(), inp.end(), ' '), inp.end());
+    return inp;
+  };
 };
