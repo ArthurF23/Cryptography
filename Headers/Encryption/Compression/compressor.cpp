2245646b7714a76490d4017eb0d5dc6f802786f9
Arthur <71910656+ArthurF23@users.noreply.github.com>
2022-02-17 15:25:02 +0000

    Made file compression compress more

--- Headers/Encryption/Compression/compressor.cpp
+++ Headers/Encryption/Compression/compressor.cpp
@@ -23,9 +23,30 @@ namespace COMPRESSION {
     {"17", "Q"},
     {"18", "R"},
     {"19", "S"},
-    {"20", "T"}
+
+    {"20", "T"},
+    {"30", "U"},
+    {"40", "V"},
+    {"50", "W"},
+    {"60", "X"},
+    {"70", "Y"},
+    {"80", "Z"},
+    {"90", "@"}
     };
-  //Roughly ~20% compression ratio on the bee movie script, my accurate representation of a lot of data. The original binary length was 437376 and was compressed to 328840 characters long.
+
+  const string binary_compression::thirdLayerMulRChars[thirdLayerMultiplier[0]][thirdLayerMultiplier[1]] = {
+    {"0", "."},
+    {"1", ","},
+    {"2", "}"},
+    {"3", "]"},
+    {"4", "["},
+    {"5", "{"},
+    {"6", "%"},
+    {"7", "*"},
+    {"8", "&"},
+    {"9", "$"}
+    };
+  //Roughly ~20% compression ratio on the bee movie script, my accurate representation of a lot of data. The original binary length was 437376 and was compressed to 328840 characters long. Further compressed to 178051 characters long, thats 40% the size of the original. THe third layer of compression got the size down to 176128. Thats only .1% more compressed.
   string binary_compression::compress(string input) {    
     string str;
 
@@ -65,14 +86,53 @@ namespace COMPRESSION {
           str+=input[i];
         }
       }
-    }
+    };
+    
+    input = str;
+    str.clear();
+    
+    //Third layer of compression
+    for (int i = 0, increment = 1; i < input.length(); i++) {
+      for (increment = 1;; increment++) {
+        if (input[i] != input[i+increment] || increment == 9) {break;};
+      };
+      
+      if (increment > 1) {
+        str+=input[i] + thirdLayerMulRChars[increment][1];
+        i += increment-1;
+      }
+      else {
+        str += input[i];
+      };
+      increment = 1;      
+    };
+
     return str + breakChar;
   };
 
   string binary_compression::decompress(string input) {
     string output;
 
-    //Letter decompression
+    //Third Layer Decompression
+    for (int i = 0, pos = 0; i < input.length(); i++) {
+      if (input[i] == breakChar) {output+=breakChar; break;}
+      for (int x = 0; x < thirdLayerMultiplier[0]; x++) {
+        if (input[i] == (thirdLayerMulRChars[x][1][0])) {pos = x; break;};
+      };
+      if (pos > 1) {
+        int rep = thirdLayerMulRChars[pos][0][0]-'0';
+        for (int x = 0; x < rep-1; x++) {
+          output+=input[i-1];
+        };
+      }
+      else {
+        output+=input[i];
+      };
+    };
+    input = output;
+    output.clear();
+
+    //Second layer decompression
     for (int i = 0; i < input.length(); i++) {
       for (int x = 0; x < rCharLength[0]; x++) {
         if (replacementChars[x][1][0] == input[i]) {
